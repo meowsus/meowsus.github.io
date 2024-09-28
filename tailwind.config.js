41b58c12fcb9d664ccd9b6568a966e5a2145b772
meowsus <meowsus@users.noreply.github.com>
2024-09-28 15:48:42 +0000

    deploy: a1855222ac13701b0d2f6c60deea449e184205d9

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   content: [
-    './_drafts/**/*.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.md',
-    './*.html',
+    "./_drafts/**/*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.md",
+    "./*.html",
   ],
   theme: {
     theme: {
       extend: {},
     },
   },
-  plugins: []
-}
+  plugins: [require("@tailwindcss/typography")],
+};
