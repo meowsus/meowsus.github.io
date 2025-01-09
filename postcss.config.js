7cb2f8263efe96ad30d3363f718d9de3655c0d1a
meowsus <meowsus@users.noreply.github.com>
2025-01-09 18:37:35 +0000

    deploy: bef5c6127a12f59b071aeacdf5faf11d692915a7

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
+    require("postcss-import"),
     ...(process.env.JEKYLL_ENV == "production"
       ? [require("cssnano")({ preset: "default" })]
       : []),
